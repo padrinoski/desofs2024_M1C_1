8c93dacc59b20e5004d5dc78664210eb00c76450
Leandro <1190800@isep.ipp.pt>
2024-05-17 12:58:35 +0100

    Migração

--- DESOFT/DESOFT.Server/API/Infrastructure/Migrations/20240515230500_Adiciona_shoppingCartId_a_ShoppingCartItem.Designer.cs
+++ DESOFT/DESOFT.Server/API/Infrastructure/Migrations/20240515230500_Adiciona_shoppingCartId_a_ShoppingCartItem.Designer.cs
@@ -4,6 +4,7 @@ using DESOFT.Server.API.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DESOFT.Server.API.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240515230500_Adiciona_shoppingCartId_a_ShoppingCartItem")]
+    partial class Adiciona_shoppingCartId_a_ShoppingCartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DESOFT/DESOFT.Server/API/Infrastructure/Migrations/20240515230500_Adiciona_shoppingCartId_a_ShoppingCartItem.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DESOFT.Server.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adiciona_shoppingCartId_a_ShoppingCartItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ShoppingCartId",
+                table: "ShoppingCartItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "ID of the Shopping Cart");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItem_ShoppingCartId",
+                table: "ShoppingCartItem",
+                column: "ShoppingCartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItem_ShoppingCart_ShoppingCartId",
+                table: "ShoppingCartItem",
+                column: "ShoppingCartId",
+                principalTable: "ShoppingCart",
+                principalColumn: "ShoppingCartId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItem_ShoppingCart_ShoppingCartId",
+                table: "ShoppingCartItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingCartItem_ShoppingCartId",
+                table: "ShoppingCartItem");
+
+            migrationBuilder.DropColumn(
+                name: "ShoppingCartId",
+                table: "ShoppingCartItem");
+        }
+    }
+}
