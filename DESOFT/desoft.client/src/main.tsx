99281ac7c59b42179500335b566320f8e4e55604
Leandro <1190800@isep.ipp.pt>
2024-06-01 17:36:35 +0100

    Correções de layout e CORS

--- DESOFT/desoft.client/src/main.tsx
+++ DESOFT/desoft.client/src/main.tsx
@@ -56,7 +56,7 @@ const router = createBrowserRouter([
         axios.defaults.headers.common['Authorization'] = token;
     } else {
         axios.defaults.headers.common['Authorization'] = null;
-        /*if setting null does not remove `Authorization` header then try     
+        /*if setting null does not remove `Authorization` header then try
           delete axios.defaults.headers.common['Authorization'];
         */
     }
