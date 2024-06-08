46359ec24194564567a0142b6c862a2ef7e12366
Guilherme Cota <1190640@isep.ipp.pt>
2024-06-08 21:50:10 +0100

    #15 #16 Added auth0 access token to allow for users to make requests to our local api

--- DESOFT/desoft.client/src/Profile/Profile.tsx
+++ DESOFT/desoft.client/src/Profile/Profile.tsx
@@ -1,11 +1,51 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import {React, useEffect} from "react"
+import { useState } from "react";
 
 const Profile = () => {
-    const {user, isAuthenticated, isLoading} = useAuth0();
+    const {user, isAuthenticated, isLoading, getAccessTokenSilently} = useAuth0();
+    const [userMetadata, setUserMetadata] = useState(null);
+
+    useEffect(() => {
+        const getUserMetadata = async () => {
+            const domain = "localhost:5265";
+            
+            try{
+                const accessToken = await getAccessTokenSilently({
+                    authorizationParams: {
+                        audience: `http://${domain}/api/`,
+                        scope: "read:current_user",
+                    },
+                })
+                console.log("User sub: " + user?.sub);
+                const userDetailsByIdUrl = `http://${domain}/api/users/${user.sub}`;
+
+                const metadataResponse = await fetch(userDetailsByIdUrl, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+
+                    },
+                });
+
+                const {user_metadata} =  await metadataResponse.json();
+
+                setUserMetadata(user_metadata);
+
+            } catch(e){
+                console.log(e.message);
+            }
+        };
+        if (user && isAuthenticated) {
+            getUserMetadata();
+        }
+
+    }, [user, getAccessTokenSilently]);
+
 
     if(isLoading){
         return <div>Loading ...</div>
     }
+
 
     return(
         isAuthenticated && (
@@ -13,6 +53,9 @@ const Profile = () => {
                 <img src={user?.picture} alt={user?.name}></img>
                 <h2>{user?.name}</h2>
                 <p>{user?.email}</p>
+                <p>{user?.roles}</p>
+                <h3>User Metadata</h3>
+                {userMetadata ? (<pre>{JSON.stringify(userMetadata, null, 2)}</pre>):("No User Metadata defined")}
             </div>
         )
     
