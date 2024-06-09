a8debeb8156d9e20d4b753e3a1b5c64799de9f63
Leandro <1190800@isep.ipp.pt>
2024-06-09 19:51:21 +0100

    update database

--- DESOFT/desoft.client/src/main.tsx
+++ DESOFT/desoft.client/src/main.tsx
@@ -22,7 +22,7 @@ import Profile from './Profile/Profile.tsx'
 (function () {
     //const token = window.sessionStorage.getItem("token");
     //token admin
-    const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2IiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.NlVN1CIDgT4ITGGpRQPkabyWOgi_VBEL0AUIpBKSW74";
+    const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhdXRoMHw2NjY0NzliNDM5YzExMmJjZTBkNTBhYWMiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.KlGWJ9lBT6OyLMdOweNuezsFv8NIKj_YWhJ-Ct5r7EU";
 
     // token frontoffice
     //const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxNiIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTUxNjIzOTAyMn0.xK9Se9DVQGOIi6411TCm6y_B5uDWQgqxl5XY9akZcp8";
